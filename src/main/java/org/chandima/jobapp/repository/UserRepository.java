146c61e18882851cc362bb5a305c500a1f64625a
chandima2000 <hychandima2000@gmail.com>
2024-10-20 12:21:49 +0530

    create the  UserRepository interface

---
+++ src/main/java/org/chandima/jobapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.chandima.jobapp.repository;
+
+
+import org.chandima.jobapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
